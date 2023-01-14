72103032c92f738b6bef15ed9d37b2dab5dede13
aKrivonogova <foxic210@gmail.com>
2023-01-14 03:24:03 +0300

    review html and css files

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 
 let editButtom = document.querySelector('.profile__edit-button');
 let profileName = document.querySelector('.profile__name');
-let popup = document.querySelector('.popup')
-
+let popup = document.querySelector('.popup');
 let profileDescriptionJob = document.querySelector('.profile__description');
-let closeButton = document.querySelector('.popup__close-button')
-
+let closeButton = document.querySelector('.popup__close-button');
 let inputProfileName = document.getElementById('userName');
 let inputDescriptionJob = document.getElementById('userJobDescription');
+let saveDataButton = document.getElementById('saveButton');
+let elemetsList = document.querySelector('.elements__list');
+
 inputProfileName.value = profileName.textContent.trim();
 inputDescriptionJob.value = profileDescriptionJob.textContent.trim();
-let saveDataButton = document.getElementById('saveButton');
+
 function handleFormSubmit(evt) {
     evt.preventDefault();
     profileName.textContent = inputProfileName.value;
@@ -25,14 +26,11 @@ function openPopup() {
     popup.classList.add('popup_opened');
 }
 
-let elemetsList = document.querySelector('.elements__list');
-
 elemetsList.querySelectorAll('.element__like').forEach(element => {
     element.addEventListener('click', () => {
         element.classList.toggle('element__like_active')
     })
 });
-
 
 saveDataButton.addEventListener('click', handleFormSubmit);
 closeButton.addEventListener('click', closePopup);
