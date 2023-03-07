e6542dbc072d52a0b8b5d244836e91ff2d593c6e
aKrivonogova <foxic210@gmail.com>
2023-03-07 17:37:25 +0300

    changed card.js

--- scripts/Card.js
+++ scripts/Card.js
@@ -15,27 +15,34 @@ class Card {
     }
 
     _isLikeCard() {
-        this._cardElement.querySelector('.element__like').classList.toggle('element__like_active');
+        this._elementLike.classList.toggle('element__like_active');
     }
     _deleteCard() {
-        this._cardElement.querySelector('.element__delete').closest('.element').remove();
+        this._cardElement.remove();
     }
     _setEventListeners() {
-        this._cardElement.querySelector('.element__like').addEventListener('click', () => {
+        this._elementLike.addEventListener('click', () => {
             this._isLikeCard();
         });
-        this._cardElement.querySelector('.element__delete').addEventListener('click', () => {
+        this._deleteElement.addEventListener('click', () => {
             this._deleteCard();
         })
-        this._cardElement.querySelector('.element__image').addEventListener('click', () => {
+        this._elementImage.addEventListener('click', () => {
             this._handlerPopupImageOpen();
         })
     }
     generateCard() {
         this._cardElement = this._getTemplate();
+        this._elementImage = this._cardElement.querySelector('.element__image');
+        this._elementLike = this._cardElement.querySelector('.element__like');
+        this._deleteElement = this._cardElement.querySelector('.element__delete');
+        this._elementName = this._cardElement.querySelector('.element__name');
+
         this._setEventListeners();
-        this._cardElement.querySelector('.element__name').textContent = this._name;
-        this._cardElement.querySelector('.element__image').src = this._link;
+
+        this._elementName.textContent = this._name;
+        this._elementImage.src = this._link;
+        this._elementImage.alt = this._name;
         return this._cardElement;
     }
     _handlerPopupImageOpen() {
